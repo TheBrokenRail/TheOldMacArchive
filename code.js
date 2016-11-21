de7e2eb975b59f3c2abb5e75201ddc91715e2e0a
TheBrokenRail <connor24nolan@live.com>
2016-11-21 15:15:43 -0500

    Basic UI

--- code.js
+++ code.js
@@ -1,2 +1,15 @@
+var state = 0;
+
 window.onload = function () {
+  var content = document.getElementById("content");
+  content.innerHTML = "";
+  if (state == 0) {
+    var input = document.createElement("INPUT");
+    var button = document.createElement("BUTTON");
+    var breakLine = document.createElement("BR");
+    button.innerHTML = "Search";
+    content.appendChild(input);
+    content.appendChild(button);
+    content.appendChild(breakLine);
+  }
 }
