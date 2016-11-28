7431dbdd43a4ecdb9e8da611a8d67e637deea0dc
TheBrokenRail <connor24nolan@live.com>
2016-11-28 15:20:12 -0500

    Update code.js

--- code.js
+++ code.js
@@ -65,7 +65,7 @@ function list(xml) {
   for (; i < xml.children.length; i++) {
     var a = document.createElement("A");
     a.id = "name";
-    a.href = "#" + xml.children[i].id;
+    // a.href = "#" + xml.children[i].id;
     a.setAttribute("onclick", "document.location = document.location.origin + document.location.pathname + " + "#" + xml.children[i].id + ";document.location.reload();");
     a.innerHTML = xml.children[i].getAttribute("name");
     var p = document.createElement("P");
